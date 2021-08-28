66cb7e993c4a83b798f5fbdf36700cba4ef8cff9
Schuyler Martin <schuylermartin45@gmail.com>
2021-08-28 14:18:15 -0600

    Added `remote` mode and better default options
    - "Default Mode" is now a lot smarter. It conditionally runs rsync based on
      the available environment variables
    - Added a new set of environment variables to support rsyncing to a remote
      server that isn't Octoprint
    - This is great as I now operate a general back-up server on my network and
      it will get the latest files as I sync the SD card
    - Remote server back-ups now sync everything, not just `*.gcode` files
    - Added a safety check to ensure the `PRINTER_FILES_DIR` environment variable
      is set.

--- README.md
+++ README.md
@@ -20,13 +20,30 @@ To make these scripts portable and easily accessible to everyone, they depend
 on a series of environment variables to be set for your custom configuration.
 
 Set these in your shell's RC file as appropriate:
+
 ```bash
-PRINTER_FILES_DIR       # Local disk directory storing printer files 
-PRINTER_SD_CARD_NAME    # Name of the SD card used with the printer. This is
-                        # not a path in case the path changes.
-OCTOPRINT_USER_NAME     # User name of Octoprint server
-OCTOPRINT_SERVER_IP     # IP address of the Octoprint server
+PRINTER_FILES_DIR           # Local disk directory storing printer files 
+PRINTER_SD_CARD_NAME        # Name of the SD card used with the printer. This is
+                            # not a path in case the path changes.
+
+# Syncs to the octoprint default directory
+OCTOPRINT_USER_NAME         # User name of Octoprint server
+OCTOPRINT_SERVER_IP         # IP address of the Octoprint server
+
+# Remote servers will back-up all files
+SSH_REMOTE_USER_NAME        # User name of remote server (that you have SSH
+                            # access too)
+SSH_REMOTE_SERVER_IP        # IP Address of remote server
+SSH_REMOTE_FILES_DIR        # Directory on remote server
 ```
+
+Defining all required environment variables for an operating mode ensures the
+mode will be run by default. This can be overridden by explicitly stating
+a single mode.
+
+For example, defining `SSH_REMOTE_*` and `PRINTER_*` will sync to those
+destinations, but ignore syncing to an Octoprint server.
+
 
 ## sync3d
 Syncs directory of 3D printer files to an SD card and/or Octoprint instance.
@@ -34,5 +51,5 @@ By default, this attempts to `rsync` to both.
 
 ### Usage
 ```bash
-./sync3d.sh [sd|octo]
+./sync3d.sh [sd|octo|remote]
 ```
--- sync3d.sh
+++ sync3d.sh
@@ -6,20 +6,24 @@
 # @author Schuyler Martin <https://github.com/schuylermartin45>
 #
 
-declare -r USAGE="USAGE: ./sync3d.sh [sd|octo]"
-declare -A OPTS=( ["sd"]=0 ["octo"]=1 )
+declare -r USAGE="USAGE: ./sync3d.sh [sd|octo|remote]"
+declare -A OPTS=( ["sd"]=0 ["octo"]=1 ["remote"]=2 )
 
 # Wraps rsync to target a specific SRC and DST.
 # 
 # $1: Source directory
 # $2: Destination directory
+# $3: Inclusion file pattern
 function syncWrapper {
+    src="${1}"
     # Ensure trailing `/` on the source directory so that the destination is
     # copied-to directly (i.e. `files/*` -> `dst/` creates `dst/*` not
     # `dst/files/*`)
-    src="${1}"
     [[ "${src}" != */ ]] && src="${src}/"
-    rsync -zarvm --delete-excluded --modify-window=2 --include "*/" --include="*.gcode" --exclude="*" "${src}" "${2}"
+    include="${3}"
+    # Default to copying only gcode files
+    [[ -z "${include}" ]] && include="*.gcode"
+    rsync -zarvm --delete-excluded --modify-window=2 --include "*/" --include="${include}" --exclude="*" "${src}" "${2}"
 }
 
 ####    MAIN     ####
@@ -33,20 +37,31 @@ function main {
         echo ${USAGE}
         exit 2
     fi
+    if [[ -z "${PRINTER_FILES_DIR}" ]]; then
+        echo "Environment variable 'PRINTER_FILES_DIR' must be set."
+        exit 4
+    fi
 
     # Calc paths
     declare -r sdMountPt=$(mount | grep "${PRINTER_SD_CARD_NAME}" | awk '{ print $3 }')
     declare -r sdHwdPath=$(mount | grep "${PRINTER_SD_CARD_NAME}" | awk '{ print $1 }')
     declare -r octoDstDir="${OCTOPRINT_USER_NAME}@${OCTOPRINT_SERVER_IP}:~/.octoprint/uploads/"
+    declare -r remoteDstDir="${SSH_REMOTE_USER_NAME}@${SSH_REMOTE_SERVER_IP}:${SSH_REMOTE_FILES_DIR}"
     
-    # Bail if no path is found, as a safety measure
-    if [ "${1}" != "octo" ] && [ ! -d "${sdMountPt}" ]; then
-        echo "Failed to find SD card named '${PRINTER_SD_CARD_NAME}'"
-        exit 3
-    fi
+    # Concat all the environment variables in a subset into one string to act
+    # act as a boolean check later.
+    declare -r hasSD="${PRINTER_SD_CARD_NAME}"
+    declare -r hasOcto="${OCTOPRINT_USER_NAME}${OCTOPRINT_SERVER_IP}"
+    declare -r hasRemote="${SSH_REMOTE_USER_NAME}${SSH_REMOTE_SERVER_IP}${SSH_REMOTE_FILES_DIR}"
 
-    # Sync based on the provided flags (by negating against the other flag)
-    if [ "${1}" != "octo" ]; then
+    # Sync based on available environment vars and override flags
+    if [[ "${1}" == "sd" ]] || [[ -z "${1}" && ! -z "${hasSD}" ]]; then
+        # Bail if no path is found, as a safety measure
+        if [ ! -d "${sdMountPt}" ]; then
+            echo "Failed to find SD card named '${PRINTER_SD_CARD_NAME}'"
+            exit 3
+        fi
+
         echo "Syncing to SD card..."
         syncWrapper "${PRINTER_FILES_DIR}" "${sdMountPt}"
         if command -v udisksctl &> /dev/null; then
@@ -55,9 +70,13 @@ function main {
             echo "########## SD UNMOUNTED. SAFE TO REMOVE! ##########"
         fi
     fi
-    if [ "${1}" != "sd" ]; then
+    if [[ "${1}" == "octo" ]] || [[ -z "${1}" && ! -z ${hasOcto} ]]; then
         echo "Syncing up to Octoprint..."
         syncWrapper "${PRINTER_FILES_DIR}" "${octoDstDir}"
+    fi
+    if [[ "${1}" == "remote" ]] || [[ -z "${1}" && ! -z ${hasRemote} ]]; then
+        echo "Syncing up to remote server..."
+        syncWrapper "${PRINTER_FILES_DIR}" "${remoteDstDir}" "*"
     fi
     echo "Done!"
 }
